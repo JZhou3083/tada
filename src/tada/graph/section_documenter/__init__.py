5d13f24efcfaa4196237c166196cfef1f6c7fa3a
Alex Simcock <asimcock@jaguarlandrover.com>
2026-05-22 12:24:26 +0100

    chore: specify public API for graph modules in line with duo recommendations

---
+++ src/tada/graph/section_documenter/__init__.py
@@ -0,0 +1,3 @@
+from tada.graph.section_documenter.graph import build_section_documenter_subgraph
+
+__all__ = ["build_section_documenter_subgraph"]
---
+++ src/tada/graph/workbook_documenter/__init__.py
@@ -0,0 +1,3 @@
+from tada.graph.workbook_documenter.graph import build_documentation_workflow
+
+__all__ = ["build_documentation_workflow"]
