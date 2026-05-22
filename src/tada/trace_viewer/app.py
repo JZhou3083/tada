625e78ee0ca97325bca1b47cb52508a6a2175cfb
Alex Simcock <asimcock@jaguarlandrover.com>
2026-05-22 13:11:04 +0100

    chore: correct new import for console error printing in view-traces

--- src/tada/trace_viewer/app.py
+++ src/tada/trace_viewer/app.py
@@ -4,7 +4,7 @@ import typer
 from rich.console import Console
 from rich.panel import Panel
 
-from tada.cli.display.errors import print_typer_error
+from tada.cli.display.console import print_typer_error
 from tada.runtime.context import RUNS_DIR
 from tada.settings import settings
 from tada.trace_viewer._optional import OptionalDependencyError
